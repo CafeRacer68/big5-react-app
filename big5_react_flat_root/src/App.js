1aa9a28b7e60648961e588302efb545e6454a9c5
CafeRacer68 <thecaferaceralliance@gmail.com>
2025-05-30 06:59:23 +0000

    update home page

--- big5_react_flat_root/src/App.js
+++ big5_react_flat_root/src/App.js
@@ -44,12 +44,48 @@ function Home() {
           style={{ width: "200px", height: "auto" }}
         />
       </div>
-      <h1>
-        Welcome back to the Big 5, {userName} 👋 The Big 5 App will help you do
-        more of the things that are good for your mental health.{" "}
-      </h1>
-      <MoodCheckin />
+
+      <h1>Welcome back to the Big 5, {userName} 👋</h1>
+
+      {/* Intro text */}
+      <p style={{ marginTop: 10 }}>
+        The Big 5 App will help you do more of the things that are good for your mental health.
+      </p>
+
+      {/* How does it work section */}
+      <div style={{ margin: "30px 0" }}>
+        <h2>How Does It Work?</h2>
+        <p>
+          It’s simple. Each weekday we will send you a single ‘nudge’.<br />
+          Each nudge is about one of the Big 5 actions.<br />
+          We know that people who do more of the Big 5 start to feel better within a week.<br />
+          We hope this will help you.
+        </p>
+      </div>
+
       <DailyMessage />
+      <MoodCheckin />
+
+      {/* Big 5 Quiz section */}
+      <div style={{ margin: "40px 0 20px" }}>
+        <h2>Big 5 Quiz</h2>
+        <p>
+          To check your own Big 5 scores, click below. This is quick and easy and will give you feedback about things you might want to consider.
+        </p>
+        <button
+          onClick={() => window.open("https://app.mindspot.org.au/v3/big5?website=true", "_blank")}
+          style={{
+            padding: "10px 20px",
+            backgroundColor: "#003366",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            marginTop: "10px",
+          }}
+        >
+          Big 5 Checklist
+        </button>
+      </div>
 
       {/* Big 5 Accordion */}
       <div style={{ marginTop: "30px" }}>
@@ -118,26 +154,6 @@ Plan social activities that make you feel supported and uplifted.`,
             </p>
           </details>
         ))}
-      </div>
-
-      {/* Big 5 Checklist Button */}
-      <div style={{ marginTop: "30px", textAlign: "center" }}>
-        <a
-          href="https://app.mindspot.org.au/v3/big5?website=true"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{
-            display: "inline-block",
-            backgroundColor: "#003366",
-            color: "white",
-            padding: "10px 15px",
-            borderRadius: "5px",
-            textDecoration: "none",
-            marginBottom: "20px",
-          }}
-        >
-          📝 Big 5 Checklist
-        </a>
       </div>
 
       <MoodHistoryChart />
