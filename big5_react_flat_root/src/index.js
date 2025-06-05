093064aa3f28f110deacfd7a2fbe40c41c339272
CafeRacer68 <thecaferaceralliance@gmail.com>
2025-06-05 05:10:04 +0000

    commit

--- big5_react_flat_root/src/index.js
+++ big5_react_flat_root/src/index.js
@@ -14,3 +14,15 @@ serviceWorkerRegistration.register({
     console.log("New content is available:", registration);
   },
 });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/firebase-messaging-sw.js")
+      .then((registration) => {
+        console.log("✅ Service Worker registered:", registration);
+      })
+      .catch((err) => {
+        console.error("❌ Service Worker registration failed:", err);
+      });
+  });
+}
